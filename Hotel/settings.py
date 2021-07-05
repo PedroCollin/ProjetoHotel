39e16fbb79554af9ea2a571be5e542175d3558b6
ElielPimenta <elielhsp@gmail.com>
2021-07-05 12:01:58 -0300

    TESTANDO commit

--- Hotel/settings.py
+++ Hotel/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$um)eyo8fq4of!51#(=-fuu9am(^r+tj!_f4-4$xxj=ootrl*h'
+SECRET_KEY = 'django-insecure-vi0n-$9r*573fst=&0y$(3(5*=hi6&r$^*rgvr$1&zi(2jd5@p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
